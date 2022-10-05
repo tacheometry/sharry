c47af82a4332f92d7bcf6517577b6749c3728cbf
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2022-10-05 06:33:26 +0000

    Update tus-js-client to 1.8.0-2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   val SourcecodeVersion = "0.2.8"
   val SwaggerVersion = "4.14.2"
   val TikaVersion = "2.5.0"
-  val TusClientVersion = "1.8.0-1"
+  val TusClientVersion = "1.8.0-2"
   val YamuscaVersion = "0.9.0"
 
   val scribe = Seq(
